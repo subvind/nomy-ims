a2d72e8acf2d482f1ad2f158949ef6b9b452f486
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-12-26 16:40:52 -0600

    htmx

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Req, Res, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,41 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index') // 'index' corresponds to the name of your view file without extension
+  getIndex() {
+    // You can add some data here if needed
+    return {
+      title: 'enigma machine'
+    };
   }
+
+  @Get('message')
+  @Render('message')
+  getMessage() {
+    return {};
+  }
+
+
+  @Get('rotors')
+  @Render('rotors')
+  getRotors() {
+    return {};
+  }
+
+  @Post('broadcast')
+  postData(@Req() req, @Res() res) {
+    // Handle your POST request data
+    const channel = req.body.channel || 'No channel received';
+
+    // You can process the data here and send a response
+    const response = `Received data: ${channel}`;
+
+    // Send the response back
+    res.send(response);
+  }
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // Set the view engine and the views folder
+  app.setViewEngine('ejs');
+  app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
+
   await app.listen(3000);
 }
+
 bootstrap();
